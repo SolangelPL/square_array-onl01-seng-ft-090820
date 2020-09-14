aea09a19d9ff986f0518004e4e96f1f72989d2b3
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-14 15:29:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,9 @@
+
+
+
 def square_array(array)
   array.collect do |numbers|
   numbers * numbers
   end 
 end 
   
-def square_array(array)
-  array.each do |number|
-  numbers * numbers 
-  end
-end
